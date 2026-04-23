35a56d768e2b141bbd13371b6b13cc4a991595c2
paylive91 <57781421-paylive91@users.noreply.replit.com>
2026-04-23 12:48:12 +0000

    Point frontend to backend API

--- artifacts/trustpay/src/main.tsx
+++ artifacts/trustpay/src/main.tsx
@@ -1,10 +1,11 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { setAuthTokenGetter } from "@workspace/api-client-react";
+import { setAuthTokenGetter, setBaseUrl } from "@workspace/api-client-react";
 import { getAuthToken } from "./lib/auth";
 import { capturePWAInstallPrompt } from "./lib/pwa-install";
 
+setBaseUrl("https://trustpay-api.onrender.com");
 setAuthTokenGetter(() => getAuthToken());
 capturePWAInstallPrompt();
 
